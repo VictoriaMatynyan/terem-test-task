355b29959a9a71bc819c4f1c5418f20e5ce57634
Victoria Matynyan <victoria.matynyan@gmail.com>
2024-03-29 15:59:52 +0300

    refactoring: task-2 - reqrite jQuery to JS, use custom html5 form components instead of bootstrap to use FormData object, complete task 2.6 for async request with form data params

--- task-2/dropdown.js
+++ task-2/dropdown.js
@@ -1,34 +1,37 @@
 const form = document.querySelector('.form');
-const jsonOutput = document.querySelector('.json__data');
 
 // задание 5 - формирование JSON-структуры из данных формы и вывод её на экран
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', async (e) => {
     e.preventDefault();
+    const jsonOutput = document.querySelector('.json__data');
     const jsonData = {};
     const formData = new FormData(form);
 
-    for (const [key, value] of formData.entries()) {
+    for (const [key, value] of formData) {
         jsonData[key] = value;
     }
 
-    jsonOutput.textContent = JSON.stringify(jsonData, null, 2);
-    document.querySelector('.json__header').textContent = 'JSON данные';
-})
+    jsonOutput.textContent = JSON.stringify(jsonData);
 
-
-// задание 6 - отправляем GET-запрос с полученными данными (примерная схема)
-// const url = 'url';
-// try {
-//     const response = await fetch(url, {
-//         method: 'GET',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             Accept: 'application/json'
-//         },
-//         body: jsonData
-//     });
-//     const data = await response.json();
-//     alert(data);
-// } catch (err) {
-//     console.error('Произошла ошибка:', err);
-// }
+    // задание 6 - отправляем GET-запрос с полученными данными
+    const currentUrl = window.location.href;
+    const params = new URLSearchParams(jsonData).toString();
+    const baseUrl = `${currentUrl}?${params}`; // к текущему url добавляем данные из формы
+    try {
+        const res = await fetch(baseUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/javascript',
+                Accept: 'application/javascript'
+            },
+        });
+        if (res.ok) {
+            const data = await res.text();
+            alert(JSON.stringify(jsonData));
+        } else {
+            alert('Ошибка: ' + res.statusText);
+        }
+    } catch (err) {
+        console.error('Произошла ошибка:', err);
+    }
+});
